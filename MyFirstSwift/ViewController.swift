7a29238cffd9fc55e211601501d18e4ffd5bfffe
YangHyejin <yanghj0921@gmail.com>
2018-08-03 20:44:57 +0900

    Initial Commit

---
+++ MyFirstSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstSwift
+//
+//  Created by Yang Hyejin on 2018. 8. 3..
+//  Copyright © 2018년 Yang Hyejin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
